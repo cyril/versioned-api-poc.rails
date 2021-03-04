7ccb485ec2e1f20daab8a31217d1249270c46e88
Cyril Kato <cyril.kato@data.gouv.fr>
2021-03-04 15:17:38 +0100

    feat(request): force JSON format

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :companies
+  scope constraints: ::Route::ApiConstraints.new do
+    resources :companies
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Companies", type: :request do
+  let(:headers) do
+    { "ACCEPT" => "application/json" }
+  end
+
   describe "GET /companies" do
+    before do
+      get companies_path, headers: headers
+    end
+
     it "works!" do
-      get companies_path
       expect(response).to have_http_status(200)
     end
   end
